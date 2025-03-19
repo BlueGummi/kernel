278ebbff02902f75e89a8652b85705bebf8cace6
gummi <105593618+BlueGummi@users.noreply.github.com>
2025-03-19 16:57:43 -0500

    system  headers

--- kernel/src/system/alloc.h
+++ kernel/src/system/alloc.h
@@ -1,5 +1,6 @@
 void init_physical_allocator();
 void *alloc_page();
+void *allocate_page();
 void free_page(void *addr);
 void *alloc_pages(size_t count);
 void free_pages(void *addr, size_t count);
--- kernel/src/system/printf.h
+++ kernel/src/system/printf.h
@@ -1,4 +1,5 @@
 #include <flanterm/backends/fb.h>
 #include <flanterm/flanterm.h>
 void k_printf(const char *format, ...);
+void panic(const char *format, ...);
 void k_printf_init(struct flanterm_context *f);
